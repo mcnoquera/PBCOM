79e0becca8732bfc8c0de46f782c4daf557909ba
Reggie Cabalo <reggiecabalo@gmail.com>
2016-06-15 10:40:44 +0800

    Added files for the Enrollment feature and created finger print scanner for login

--- PBCOM Mobile/LoginViewController.swift
+++ PBCOM Mobile/LoginViewController.swift
@@ -7,12 +7,15 @@
 //
 
 import UIKit
+import LocalAuthentication
 
 class LoginViewController: UIViewController {
     
     
     @IBOutlet weak var textFieldUserID : (UITextField)!
     @IBOutlet weak var textFieldPassword: (UITextField)!
+    
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -60,6 +63,122 @@ class LoginViewController: UIViewController {
         self.dismissViewControllerAnimated(true, completion: nil)
     }
     
+    @IBAction func fingerScanLogin(sender: UIButton) {
+        
+        let authenticationContext = LAContext()
+        
+        
+        var error:NSError?
+        
+        // 2. Check if the device has a fingerprint sensor
+        // If not, show the user an alert view and bail out!
+        guard authenticationContext.canEvaluatePolicy(.DeviceOwnerAuthenticationWithBiometrics, error: &error) else {
+            
+            showAlertViewIfNoBiometricSensorHasBeenDetected()
+            return
+        }
+        
+        authenticationContext.evaluatePolicy(
+            .DeviceOwnerAuthenticationWithBiometrics,
+            localizedReason: "Only awesome people are allowed",
+            reply: { [unowned self] (success, error) -> Void in
+                
+                if( success ) {
+                    
+                    // Fingerprint recognized
+                    // Go to view controller
+                    self.navigateToAuthenticatedViewController()
+                    
+                    print("Finger Print Key: \(authenticationContext.evaluatedPolicyDomainState!.base64EncodedStringWithOptions([]))")
+                    
+                }else {
+                    
+                    // Check if there is an error
+                    if let error = error {
+                        
+                        let message = self.errorMessageForLAErrorCode(error.code)
+//                        self.showAlertViewAfterEvaluatingPolicyWithMessage(message)
+                        self.showAlertWithTitle("", message: (message))
+                        
+                    }
+                    
+                }
+                
+            })
+    }
+    
+    func errorMessageForLAErrorCode( errorCode:Int ) -> String{
+        
+        var message = ""
+        
+        switch errorCode {
+            
+        case LAError.AppCancel.rawValue:
+            message = "Authentication was cancelled by application"
+            
+        case LAError.AuthenticationFailed.rawValue:
+            message = "The user failed to provide valid credentials"
+            
+        case LAError.InvalidContext.rawValue:
+            message = "The context is invalid"
+            
+        case LAError.PasscodeNotSet.rawValue:
+            message = "Passcode is not set on the device"
+            
+        case LAError.SystemCancel.rawValue:
+            message = "Authentication was cancelled by the system"
+            
+        case LAError.TouchIDLockout.rawValue:
+            message = "Too many failed attempts."
+            
+        case LAError.TouchIDNotAvailable.rawValue:
+            message = "TouchID is not available on the device"
+            
+        case LAError.UserCancel.rawValue:
+            message = "The user did cancel"
+            
+        case LAError.UserFallback.rawValue:
+            message = "The user chose to use the fallback"
+            
+        default:
+            message = "Did not find error code on LAError object"
+            
+        }
+        
+        return message
+        
+    }
+    
+    
+ 
+    func navigateToAuthenticatedViewController(){
+        
+        loginAction(UIButton)
+    }
+    
+    func showAlertViewIfNoBiometricSensorHasBeenDetected(){
+        
+        showAlertWithTitle("Error", message: "This device does not have a TouchID sensor.")
+        
+    }
+    
+    func showAlertWithTitle( title:String, message:String ) {
+        
+        let alertVC = UIAlertController(title: title, message: message, preferredStyle: .Alert)
+        
+        let okAction = UIAlertAction(title: "Ok", style: .Default, handler: nil)
+        alertVC.addAction(okAction)
+        
+        dispatch_async(dispatch_get_main_queue()) { () -> Void in
+            
+            self.presentViewController(alertVC, animated: true, completion: nil)
+            
+        }
+        
+    }
+    
+    
+    
 }
 
 extension LoginViewController: UITextFieldDelegate {
@@ -71,8 +190,8 @@ extension LoginViewController: UITextFieldDelegate {
     }
     
     func textFieldShouldReturn(textField: UITextField) -> Bool {
-        textField.resignFirstResponder()
         
+        textField.resignFirstResponder()
         return true
         
     }
